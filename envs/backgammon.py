b6d39943aaa8cd840bfb83f516ca985a9e16bb93
Jean Cyrus de Gourcuff <jean.de-gourcuff@polytechnique.edu>
2020-03-06 09:46:27 +0100

    f

--- envs/backgammon.py
+++ envs/backgammon.py
@@ -197,19 +197,18 @@ class BlackAgent():
 
   def possible_moves(self, state, dice):
     moves = []
-    if state.barred['black'] == 0:
-      for i in range(dice, 24):
-        if state.board[i - dice] in [i for i in range(9)] and state.board[i] > 0:
-          moves.append((i, dice, 0))
-        if state.board[i - dice] == -1 and state.board[i] > 0:
-          moves.append((i, dice, -1))
-      if state.end_part:
-        for i in range(0, dice):
-          if state.board[i] > 0:
-            moves.append((i, dice, 1))
-      if moves == []:
-        moves.append((0, 0, 0))
-    else:
+    for i in range(dice, 24):
+      if state.board[i - dice] in [i for i in range(9)] and state.board[i] > 0:
+        moves.append((i, dice, 0))
+      if state.board[i - dice] == -1 and state.board[i] > 0:
+        moves.append((i, dice, -1))
+    if state.end_part:
+      for i in range(0, dice):
+        if state.board[i] > 0:
+          moves.append((i, dice, 1))
+    if moves == []:
+      moves.append((0, 0, 0))
+    if state.barred['black'] != 0:
       if state.board[24 - dice] >= 0:
         moves.append((24, dice, 0))
       if state.board[24 - dice] == -1:
